e82a2c49bae5dcb1033a975ad56d752d3aea86fb
Luis Pedro Coelho <luis@luispedro.org>
2019-03-11 05:47:57 +0100

    ENH Add sub-command API

--- nixml/main.py
+++ nixml/main.py
@@ -1,21 +1,36 @@
 import generate
+from sys import exit
 
 try:
     import argparse
 except ImportError:
     print("argparse not found. Please install argparse with 'pip install argparse'")
-    sys.exit(1)
+    exit(1)
 
 def main(args):
     import yaml
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--cache-git", action="store_true",
             help="Cache git download: will be slow the first time, but will make it faster to switch between different snapshots")
+    sp = parser.add_subparsers()
+    parser_generate = sp.add_parser('generate', help='Generate .nix file')
+    parser_generate.set_defaults(sub='generate')
+    parser_shell = sp.add_parser('shell', help='Create a shell')
+    parser_shell.set_defaults(sub='shell')
+    parser_shell.add_argument('--pure', action='store_true',
+            help='Run a pure shell, isolated from the rest of your system')
+
     opts = parser.parse_args(args)
+
     with open('env.nml') as ifile:
         data = yaml.load(ifile)
-    with open('nixml.nix', 'w') as output:
+
+    nix_file = 'nixml.nix'
+    with open(nix_file, 'w') as output:
         generate.write_nix(data, output, opts)
+    if opts.sub == 'shell':
+        import subprocess
+        exit(subprocess.call(['nix-shell', nix_file]))
 
 if __name__ == '__main__':
     from sys import argv
