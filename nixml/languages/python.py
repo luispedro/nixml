88128523070a3c8cb889e6788fcce439c53a1fab
Luis Pedro Coelho <luis@luispedro.org>
2019-03-15 03:53:37 +0100

    MIN Allow for more natural Python versions

--- nixml/languages/python.py
+++ nixml/languages/python.py
@@ -14,6 +14,16 @@ def generate(data, output, _):
     '''
     Generate a Python group
     '''
+    py_version = str(data['version'])
+    py_version = {
+            '2.7': '2',
+            '3': '36',
+            '3.4': '34',
+            '3.5': '35',
+            '3.6': '36',
+            '3.7': '37',
+            '3.8': '38',
+            }.get(py_version, py_version)
     packages = '\n    '.join(data['modules'])
-    output.write(python_group.format(py_version=data['version'], packages=packages))
+    output.write(python_group.format(py_version=py_version, packages=packages))
     return ['pwp']
