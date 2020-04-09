14082b3f3861d4f143dac74467f1b255968cc154
Luis Pedro Coelho <luis@luispedro.org>
2020-04-09 13:04:19 +0200

    ENH Add synonyms for better UX
    If the user types "sklearn" or "scikit-learn" instead of "scikitlearn",
    accept it but warn them.

--- nixml/languages/python.py
+++ nixml/languages/python.py
@@ -10,6 +10,14 @@ let
 in
 '''
 
+synonyms = {
+    'keras': ('Keras', 'package names are case sensitive'),
+
+    # Be nice to users
+    'sklearn': ('scikitlearn', None),
+    'scikit-learn': ('scikitlearn', None),
+}
+
 def generate(data, output, _):
     '''
     Generate a Python group
@@ -24,6 +32,18 @@ def generate(data, output, _):
             '3.7': '37',
             '3.8': '38',
             }.get(py_version, py_version)
-    packages = '\n    '.join(data['modules'])
+    packages = []
+    for p in data['modules']:
+        if p in synonyms:
+            np,note = synonyms[p]
+            if note is not None:
+                message = "Replacing package '{}' by '{}' ({}).".format(p, np, note)
+            else:
+                message = "Replacing package '{}' by its canonical name '{}'.".format(p, np)
+            print(message)
+            packages.append(np)
+        else:
+            packages.append(p)
+    packages = '\n    '.join(packages)
     output.write(python_group.format(py_version=py_version, packages=packages))
     return ['pwp']
